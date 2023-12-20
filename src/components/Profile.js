dd1ea6a79a2dff35157d6ad61d8d955b7866c83b
pphilippersson6 <>
2023-12-20 10:36:00 +0000

    added logout button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,22 @@
-export default function Profile() {
-    return (
-        <div>
-        <h1>Profile</h1>
-        </div>
-    );
-}
+import React from 'react';
+
+const Profile = () => {
+  const handleLogout = () => {
+
+    localStorage.removeItem('user');
+
+    window.location.href = 'src/components/Login.js';
+  };
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      {/* Your profile content goes here */}
+      
+      {/* Logout button */}
+      <button onClick={handleLogout}>Logout</button>
+    </div> 
+  );
+};
+
+export default Profile;
