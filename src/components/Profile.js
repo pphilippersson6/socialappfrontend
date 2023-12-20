dc56d8bd951e750f4704ccb8e2bf911cd6ec5863
pphilippersson6 <>
2023-12-20 10:39:29 +0000

    removed button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,22 +1,8 @@
 import React from 'react';
-
-const Profile = () => {
-  const handleLogout = () => {
-
-    localStorage.removeItem('user');
-
-    window.location.href = 'src/components/Login.js';
-  };
-
-  return (
-    <div>
-      <h1>Profile</h1>
-      {/* Your profile content goes here */}
-      
-      {/* Logout button */}
-      <button onClick={handleLogout}>Logout</button>
-    </div> 
-  );
-};
-
-export default Profile;
+export default function Profile() {
+    return (
+        <div>
+        <h1>Profile</h1>
+        </div>
+    );
+}
