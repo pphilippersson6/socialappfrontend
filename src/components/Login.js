7d977dce23773810411859ab0cf05beb12f76f75
pphilippersson6 <>
2023-12-20 11:06:30 +0000

    updated some code to make it better api

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,46 +1,58 @@
 import React from 'react';
-import "../App.css";
-import { sendPostRequest } from "../features/api";
-import Form from "./Form";
+import '../App.css';
+import { sendPostRequest } from '../features/api';
+import Form from './Form';
+import { useHistory } from 'react-router-dom';
 
 export default function Login() {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            username: e.target.username.value,
-            password: e.target.password.value
-        }
+  const history = useHistory();
 
-        sendPostRequest('/user/login/', data)
-        .then(data => {
-            console.log(data.Success);
-            if(data.Success){
-                localStorage.setItem('token', data.Details.access);
-                localStorage.setItem('user', data.Details.user_id);
-                localStorage.setItem('username', data.Details.username);
-                localStorage.setItem('email', data.Details.email);
-                window.location.href = '/';
-            }else{
-                alert(JSON.stringify(data));
-            }
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+    try {
+      const loginResponse = await sendPostRequest('/user/login/', data);
+
+      console.log(loginResponse.Success);
+
+      if (loginResponse.Success) {
+        const { access, user_id, username, email } = loginResponse.Details;
+
+        localStorage.setItem('token', access);
+        localStorage.setItem('user', user_id);
+        localStorage.setItem('username', username);
+        localStorage.setItem('email', email);
+
+        history.push('/');
+      } else {
+        alert(JSON.stringify(loginResponse));
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      alert('An error occurred. Please try again.');
     }
+  };
 
-    const children = [
-        {
-            type: "text",
-            name: "username",
-            label: "Username"
-        },
-        {
-            type: "password",
-            name: "password",
-            label: "Password"
-        }
-    ]
-    return (
-        <div className="login">
-            <Form title="Login" handleSubmit={handleSubmit} children={children} />
-        </div>
-    )
+  const children = [
+    {
+      type: 'text',
+      name: 'username',
+      label: 'Username',
+    },
+    {
+      type: 'password',
+      name: 'password',
+      label: 'Password',
+    },
+  ];
+
+  return (
+    <div className="login">
+      <Form title="Login" handleSubmit={handleSubmit} children={children} />
+    </div>
+  );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,63 +1,72 @@
 import React from 'react';
-import { sendPostRequest } from "../features/api";
-import Form from "./Form";
+import { sendPostRequest } from '../features/api';
+import Form from './Form';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup() {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const data = {
-            email: e.target.email.value,
-            username: e.target.username.value,
-            password: e.target.password.value
-        }
-        console.log(data);
+  const history = useHistory();
 
-        const resp = sendPostRequest('/user/signup/', data);
-        resp.then(data => {
-            if(data.success){
-                const loginData = {
-                    username: e.target.username.value,
-                    password: e.target.password.value
-                }
-                sendPostRequest('/user/login/', loginData)
-                .then(data => {
-                    console.log(data.Success);
-                    if(data.Success){
-                        localStorage.setItem('token', data.Details.access);
-                        localStorage.setItem('user', data.Details.user_id);
-                        localStorage.setItem('username', data.Details.username);
-                        localStorage.setItem('email', data.Details.email);
-                        window.location.href = '/';
-                    }else{
-                        alert(JSON.stringify(data));
-                    }
-                })
-            }else{
-                alert(JSON.stringify(data));
-            }
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: e.target.email.value,
+      username: e.target.username.value,
+      password: e.target.password.value,
+    };
+
+    try {
+      const signupResponse = await sendPostRequest('/user/signup/', data);
+
+      if (signupResponse.success) {
+        const loginData = {
+          username: e.target.username.value,
+          password: e.target.password.value,
+        };
+
+        const loginResponse = await sendPostRequest('/user/login/', loginData);
+
+        if (loginResponse.Success) {
+          const { access, user_id, username, email } = loginResponse.Details;
+
+          localStorage.setItem('token', access);
+          localStorage.setItem('user', user_id);
+          localStorage.setItem('username', username);
+          localStorage.setItem('email', email);
+
+          history.push('/');
+        } else {
+          alert(JSON.stringify(loginResponse));
+        }
+      } else {
+        alert(JSON.stringify(signupResponse));
+      }
+    } catch (error) {
+      console.error('Error during signup/login:', error);
+      alert('An error occurred. Please try again.');
     }
+  };
 
-    const children = [
-        {
-            type: "email",
-            name: "email",
-            label: "Email"
-        },
-        {
-            type: "text",
-            name: "username",
-            label: "Username"
-        },
-        {
-            type: "password",
-            name: "password",
-            label: "Password"
-        }
-    ]
-    return (
-        <div className="signup">
-            <Form title="Signup" handleSubmit={handleSubmit} children={children} />
-        </div>
-    )
+  const children = [
+    {
+      type: 'email',
+      name: 'email',
+      label: 'Email',
+    },
+    {
+      type: 'text',
+      name: 'username',
+      label: 'Username',
+    },
+    {
+      type: 'password',
+      name: 'password',
+      label: 'Password',
+    },
+  ];
+
+  return (
+    <div className="signup">
+      <Form title="Signup" handleSubmit={handleSubmit} children={children} />
+    </div>
+  );
 }
